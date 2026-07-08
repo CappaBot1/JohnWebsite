0117a0762243bcc521c0405b8eb2a39c9cd8c8fd
Casper Howse <casperhowse@gmail.com>
2026-07-08 16:02:38 +1200

    sprint 4: add form_submit endpoint and add email input to form

--- server.ts
+++ server.ts
@@ -3,15 +3,23 @@ import { serveDir } from "@std/http/file-server";
 const servePaths = [
     "", // this is just / which goes to index.html
     //"index.html",
+    "form_submit.html",
     "main.css",
     "main.js",
 ].map(x => "/" + x);
 
 Deno.serve((req) => {
-    const pathname = new URL(req.url).pathname;
+    const url = new URL(req.url);
+    const pathname = url.pathname;
     //console.log(pathname);
 
     if (servePaths.includes(pathname)) {
+        if (pathname == "/form_submit.html") {
+            console.log("form submission");
+            console.log(url.searchParams);
+            // TODO: add something to send the form submission to John
+        }
+        
         return serveDir(req, { fsRoot: "./" });
     }
 
