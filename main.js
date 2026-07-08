fbe3b9e798fefaa213cd85acabc4b3f09e615d55
Casper Howse <casperhowse@gmail.com>
2026-07-08 15:14:49 +1200

    sprint 2: add animations and change font

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ console.log("main.js is running at", new Date());
 
 //setTimeout(() => {location.reload()}, 1000);
 
-const doubleImages = document.querySelectorAll(".double-images img");
+const imageRowImages = document.querySelectorAll(".image-row img");
 
-doubleImages.forEach((img) => {
+imageRowImages.forEach((img) => {
     img.style.flex = img.naturalWidth / img.naturalHeight;
 });
