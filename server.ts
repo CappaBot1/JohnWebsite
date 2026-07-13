b2140119e400c0a503153b582c3375ffd78822dc
Casper Howse <casperhowse@gmail.com>
2026-07-13 16:07:19 +1200

    rename form submit page

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import { serveDir } from "@std/http/file-server";
 const servePaths = [
     "", // this is just / which goes to index.html
     //"index.html",
-    "form_submit.html",
+    "form-submit.html",
     "main.css",
     "main.js",
     "sitemap.xml",
@@ -13,10 +13,9 @@ const servePaths = [
 Deno.serve((req) => {
     const url = new URL(req.url);
     const pathname = url.pathname;
-    //console.log(pathname);
 
     if (servePaths.includes(pathname)) {
-        if (pathname == "/form_submit.html") {
+        if (pathname == "/form-submit.html") {
             console.log("form submission");
             console.log(url.searchParams);
             // TODO: add something to send the form submission to John
